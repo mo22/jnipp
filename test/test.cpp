c8058f83b70b6c3f2196aa74fb333fb48b9d9911
Moritz Moeller <mm@mxs.de>
2015-04-06 14:09:45 +0200

    ..

--- test/test.cpp
+++ test/test.cpp
@@ -14,9 +14,7 @@ void LOG(const char* fmt, ...)
 
 
 #include <jnipp.h>
-namespace jni = jnipp;
-template <typename T> using JLocalRef = jnipp::LocalRef<T>;
-using JString = jnipp::String;
+using namespace jnipp;
 
 #define EXPORT __attribute__((visibility("default"))) extern "C"
 
@@ -33,7 +31,7 @@ JNIEXPORT
 
 #include "_java.h"
 void test_string() {
-    JLocalRef<JavaLangString> str = JString::create("Hello World");
+    LocalRef<JavaLangString> str = String::create("Hello World");
 
     LOG("str=%s", str->toString()->c_str());
 
@@ -45,119 +43,67 @@ void test_string() {
     auto bytes = str->getBytes();
     LOG("bytes.length()=%d", bytes->length());
     LOG("bytes[0]=%d", bytes->operator[](0) );
-    jni::Ref<jni::Array<jbyte>> bytes2 = bytes;
+    Ref<Array<jbyte>> bytes2 = bytes;
     LOG("bytes[0]=%d", (*bytes)[0] );
     // LOG("bytes[0]=%d", bytes[0] ); // fails as expected
 
-    auto str2 = str->concat(jni::String::create(" !!"));
+    auto str2 = str->concat(String::create(" !!"));
     LOG("str2=%s", str2->toString()->c_str());
 
     //auto str3 = str->concat("sugar string");
     //LOG("str3=%s", str3->toString()->c_str());
 
-    auto parts = str->split(jni::String::create(" "));
+    auto parts = str->split(String::create(" "));
     LOG("parts.length()=%d", parts->length());
     LOG("parts[0]=%s", (*parts)[0]->toString()->c_str());
 
     LOG("String.valueOf(3.141f)=%s", JavaLangString::valueOf(3.141f)->toString()->c_str());
 
 
-    jni::StaticField<jni::String> CASE_INSENSITIVE_ORDER("java/lang/String", "CASE_INSENSITIVE_ORDER", "Ljava/util/Comparator;");
+    StaticField<String> CASE_INSENSITIVE_ORDER("java/lang/String", "CASE_INSENSITIVE_ORDER", "Ljava/util/Comparator;");
     LOG("CASE_INSENSITIVE_ORDER jfieldid %d", (jfieldID)CASE_INSENSITIVE_ORDER);
     LOG("CASE_INSENSITIVE_ORDER toString %s", CASE_INSENSITIVE_ORDER.get()->toString()->c_str());
 
     LOG("String.CASE_INSENSITIVE_ORDER %s", JavaLangString::CASE_INSENSITIVE_ORDER.get()->toString()->c_str());
     LOG("String.CASE_INSENSITIVE_ORDER %s", JavaLangString::CASE_INSENSITIVE_ORDER->toString()->c_str());
 
-    // JavaLangString::CASE_INSENSITIVE_ORDER.set(JString::create("test")); // fails with read only
+    // JavaLangString::CASE_INSENSITIVE_ORDER.set(String::create("test")); // fails with read only
 
-    JavaTest::staticObject.set( JString::create("test"));
+    JavaTest::staticObject.set( String::create("test"));
     LOG("JavaTest::staticObject %s", JavaTest::staticObject->toString()->c_str());
-    JavaTest::staticObject = (jni::Ref<jni::Object>) JString::create("test");
-    LOG("JavaTest::staticObject %s", JavaTest::staticObject->toString()->c_str());
-
-    LOG("JavaTest::staticInt %d", (jint)JavaTest::staticInt);
-    JavaTest::staticInt = 1234;
-    LOG("JavaTest::staticInt %d", (jint)JavaTest::staticInt);
-
-
-
-void test_string() {
-
-    JLocalRef<JavaLangString> str = JString::create("Hello World");
-
-    LOG("str=%s", str->toString()->c_str());
-
-    // constructors...??? must be static function...
-    // 
-    LOG("str.length()=%d", str->length());
-    LOG("str.charAt(0)=%c", str->charAt(0));
-
-    auto bytes = str->getBytes();
-    LOG("bytes.length()=%d", bytes->length());
-    LOG("bytes[0]=%d", bytes->operator[](0) );
-    jni::Ref<jni::Array<jbyte>> bytes2 = bytes;
-    LOG("bytes[0]=%d", (*bytes)[0] );
-    // LOG("bytes[0]=%d", bytes[0] ); // fails as expected
-
-    auto str2 = str->concat(jni::String::create(" !!"));
-    LOG("str2=%s", str2->toString()->c_str());
-
-    //auto str3 = str->concat("sugar string");
-    //LOG("str3=%s", str3->toString()->c_str());
-
-    auto parts = str->split(jni::String::create(" "));
-    LOG("parts.length()=%d", parts->length());
-    LOG("parts[0]=%s", (*parts)[0]->toString()->c_str());
-
-    LOG("String.valueOf(3.141f)=%s", JavaLangString::valueOf(3.141f)->toString()->c_str());
-
-
-    jni::StaticField<jni::String> CASE_INSENSITIVE_ORDER("java/lang/String", "CASE_INSENSITIVE_ORDER", "Ljava/util/Comparator;");
-    LOG("CASE_INSENSITIVE_ORDER jfieldid %d", (jfieldID)CASE_INSENSITIVE_ORDER);
-    LOG("CASE_INSENSITIVE_ORDER toString %s", CASE_INSENSITIVE_ORDER.get()->toString()->c_str());
-
-    LOG("String.CASE_INSENSITIVE_ORDER %s", JavaLangString::CASE_INSENSITIVE_ORDER.get()->toString()->c_str());
-    LOG("String.CASE_INSENSITIVE_ORDER %s", JavaLangString::CASE_INSENSITIVE_ORDER->toString()->c_str());
-
-    // JavaLangString::CASE_INSENSITIVE_ORDER.set(JString::create("test")); // fails with read only
-
-    JavaTest::staticObject.set( JString::create("test"));
-    LOG("JavaTest::staticObject %s", JavaTest::staticObject->toString()->c_str());
-    JavaTest::staticObject = (jni::Ref<jni::Object>) JString::create("test");
+    JavaTest::staticObject = (Ref<Object>) String::create("test");
     LOG("JavaTest::staticObject %s", JavaTest::staticObject->toString()->c_str());
 
     LOG("JavaTest::staticInt %d", (jint)JavaTest::staticInt);
     JavaTest::staticInt = 1234;
     LOG("JavaTest::staticInt %d", (jint)JavaTest::staticInt);
-
 
     // throw an exception
     {
-        auto ecls = jni::Class::forName("java/lang/Exception");
-        jni::Constructor<jni::Object,jni::String> method(ecls, "(Ljava/lang/String;)V");
-        auto ex = method.construct(jni::String::create("hello world!"));
-        jni::Env::throwException(ex);
-        if (jni::Env::hasException()) {
+        auto ecls = Class::forName("java/lang/Exception");
+        Constructor<Object,String> method(ecls, "(Ljava/lang/String;)V");
+        auto ex = method.construct(String::create("hello world!"));
+        Env::throwException(ex);
+        if (Env::hasException()) {
             LOG("got an exception");
-            auto ex = jni::Env::getException();
+            auto ex = Env::getException();
             LOG("ex toString: %s", ex->toString()->c_str());
         }
     }
 
     {
         // throw an exception
-        jni::Env::throwException("java/lang/RuntimeException", "message");
-        if (jni::Env::hasException()) {
+        Env::throwException("java/lang/RuntimeException", "message");
+        if (Env::hasException()) {
             LOG("got an exception");
-            auto ex = jni::Env::getException();
+            auto ex = Env::getException();
             LOG("ex toString: %s", ex->toString()->c_str());
         }
     }
 
     {
         // create byte array
-        JLocalRef<JArray<jbyte>> byteArray = JArray<jbyte>::construct(1024);
+        LocalRef<Array<jbyte>> byteArray = Array<jbyte>::construct(1024);
         LOG("byteArray.length()=%d", byteArray->length());
         for (int i=0; i<1024; i++) {
             byteArray->set(i, i & 0xFF);
@@ -169,18 +115,16 @@ void test_string() {
 
     {
         // create char array
-        auto charArray = JArray<jchar>::construct(10);
+        auto charArray = Array<jchar>::construct(10);
         for (int i=0; i<charArray->length(); i++) (*charArray)[i] = '0' + i;
         auto charString = JavaLangString::construct(charArray);
         LOG("charString=%s", charString->c_str());
     }
 };
 
-};
-
 EXPORT JNIEXPORT void JNICALL Java_Test_native1(JNIEnv* env, jobject thiz)
 {
-    jni::Env::Scope scope(env);
+    Env::Scope scope(env);
 
     test_string();
 }
